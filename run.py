1d8a4440a1c98343c1a74bf44cb1b9efc6b1a88d
Bogdan264 <bogdanmychkovskyi@gmail.com>
2023-02-14 16:13:47 +0200

    Start game and update events

--- run.py
+++ run.py
@@ -13,5 +13,21 @@ class GameController(object):
         self.background = pygame.surface.Surface(SCREENSIZE).convert()
         self.background.fill(BLACK)
 
+    def startGame(self):
+        self.setBackground()
+
+    def update(self):
+        self.checkEvents()
+        self.render()
 
 
+
+
+
+
+
+    def checkEvents(self):
+        pass
+
+    def render(self):
+        pass
