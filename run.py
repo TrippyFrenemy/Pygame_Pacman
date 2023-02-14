27e6558abe1a35cd26811e787434a2d036812629
Bogdan264 <bogdanmychkovskyi@gmail.com>
2023-02-14 16:09:39 +0200

    Setting background

--- run.py
+++ run.py
@@ -2,8 +2,16 @@ import pygame
 from pygame.locals import *
 from constants import *
 
+
 class GameController(object):
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode(SCREENSIZE, 0, 32)
         self.background = None
+
+    def setBackground(self):
+        self.background = pygame.surface.Surface(SCREENSIZE).convert()
+        self.background.fill(BLACK)
+
+
+
