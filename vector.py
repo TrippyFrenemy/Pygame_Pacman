5f3072e561593a9bd96c7dcee2f1b2c561a86564
Avruschenko Dmytro <trippyfren@gmail.com>
2023-02-14 15:59:12 +0200

    Add more arithmetic methods

--- vector.py
+++ vector.py
@@ -5,3 +5,23 @@ class Vector2(object):
         self.x = x
         self.y = y
         self.thresh = 0.000001
+
+    def __add__(self, other):
+        return Vector2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector2(self.x - other.x, self.y - other.y)
+
+    def __neg__(self):
+        return Vector2(-self.x, -self.y)
+
+    def __mul__(self, scalar):
+        return Vector2(self.x * scalar, self.y * scalar)
+
+    def __div__(self, scalar):
+        if scalar != 0:
+            return Vector2(self.x / float(scalar), self.y / float(scalar))
+        return None
+
+    def __truediv__(self, scalar):
+        return self.__div__(scalar)
