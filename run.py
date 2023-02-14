0bc50c37ba014014b98b7c082bc21ba2df881d13
Bogdan264 <bogdanmychkovskyi@gmail.com>
2023-02-14 17:06:11 +0200

    Pacman update and render on game area

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ from pygame.locals import *
 from constants import *
 
 
+from pacman import Pacman
+
+
 class GameController(object):
     def __init__(self):
         pygame.init()
@@ -16,9 +19,11 @@ class GameController(object):
 
     def startGame(self):
         self.setBackground()
+        self.pacman = Pacman()
 
     def update(self):
         dt = self.clock.tick(30) / 1000.0
+        self.pacman.update(dt)
         self.checkEvents()
         self.render()
 
@@ -28,6 +33,8 @@ class GameController(object):
                 exit()
 
     def render(self):
+        self.screen.blit(self.background, (0, 0))
+        self.pacman.render(self.screen)
         pygame.display.update()
 
 
