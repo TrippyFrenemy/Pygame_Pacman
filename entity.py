e8d5db872d1efebabb74ddb20c9ca725d21ef5bb
Avruschenko Dmytro <trippyfren@gmail.com>
2023-02-15 14:05:54 +0200

    Adding methods for random movement for ai

--- entity.py
+++ entity.py
@@ -64,3 +64,34 @@ class Entity(object):
             p = self.position.asInt()
             pygame.draw.circle(screen, self.color, p, self.radius)
 
+    def update(self, dt):
+        self.position += self.directions[self.direction] * self.speed * dt
+
+        if self.overshotTarget():
+            self.node = self.target
+            directions = self.validDirections()
+            direction = self.randomDirection(directions)
+            if not self.disablePortal:
+                if self.node.neighbors[PORTAL] is not None:
+                    self.node = self.node.neighbors[PORTAL]
+            self.target = self.getNewTarget(direction)
+            if self.target is not self.node:
+                self.direction = direction
+            else:
+                self.target = self.getNewTarget(self.direction)
+
+            self.setPosition()
+
+    def validDirections(self):
+        directions = []
+        for key in [UP, DOWN, LEFT, RIGHT]:
+            if self.validDirection(key):
+                if key != self.direction * -1:
+                    directions.append(key)
+        if len(directions) == 0:
+            directions.append(self.direction * -1)
+        return directions
+
+    def randomDirection(self, directions):
+        return directions[randint(0, len(directions) - 1)]
+
