499515397fb71946350181b794c16e4d9e783506
Bogdan264 <bogdanmychkovskyi@gmail.com>
2023-02-15 16:09:19 +0200

    Resolve problems with game area

--- pacman.py
+++ pacman.py
@@ -4,14 +4,33 @@ from vector import Vector2
 from constants import *
 from entity import Entity
 
-class Pacman(object):
+
+class Pacman(Entity):
     def __init__(self, node):
         Entity.__init__(self, node)
         self.name = PACMAN
         self.color = YELLOW
+        self.direction = LEFT
 
-    def setPosition(self):
-        self.position = self.node.position.copy()
+    def update(self, dt):
+        self.position += self.directions[self.direction] * self.speed * dt
+        direction = self.getValidKey()
+        if self.overshotTarget():
+            self.node = self.target
+            if self.node.neighbors[PORTAL] is not None:
+                self.node = self.node.neighbors[PORTAL]
+            self.target = self.getNewTarget(direction)
+            if self.target is not self.node:
+                self.direction = direction
+            else:
+                self.target = self.getNewTarget(self.direction)
+
+            if self.target is self.node:
+                self.direction = STOP
+            self.setPosition()
+        else:
+            if self.oppositeDirection(direction):
+                self.reverseDirection()
 
     def getValidKey(self):
         key_pressed = pygame.key.get_pressed()
@@ -27,9 +46,17 @@ class Pacman(object):
 
     def eatPellets(self, pelletList):
         for pellet in pelletList:
-            d = self.position - pellet.position
-            dSquared = d.magnitudeSquared()
-            rSquared = (pellet.radius + self.collideRadius) ** 2
-            if dSquared <= rSquared:
+            if self.collideCheck(pellet):
                 return pellet
         return None
+
+    def collideGhost(self, ghost):
+        return self.collideCheck(ghost)
+
+    def collideCheck(self, other):
+        d = self.position - other.position
+        dSquared = d.magnitudeSquared()
+        rSquared = (self.collideRadius + other.collideRadius) ** 2
+        if dSquared <= rSquared:
+            return True
+        return False
