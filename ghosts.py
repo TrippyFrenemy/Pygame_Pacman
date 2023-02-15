1e091a3f5cb15ceead8b4a563c0978ee2d3ad6b5
Avruschenko Dmytro <trippyfren@gmail.com>
2023-02-15 14:48:52 +0200

    Add choose random direction for ghosts

--- ghosts.py
+++ ghosts.py
@@ -9,4 +9,14 @@ class Ghost(Entity):
         Entity.__init__(self, node)
         self.name = GHOST
         self.points = 200
+        self.goal = Vector2()
+        self.directionMethod = self.goalDirection
+
+    def goalDirection(self, directions):
+        distances = []
+        for direction in directions:
+            vec = self.node.position + self.directions[direction] * TILEWIDTH - self.goal
+            distances.append(vec.magnitudeSquared())
+        index = distances.index(min(distances))
+        return directions[index]
 
