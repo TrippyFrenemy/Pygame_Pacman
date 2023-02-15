6b2525ac73f6a3c75293379c8e7f6776e8ad6899
Avruschenko Dmytro <trippyfren@gmail.com>
2023-02-15 15:47:09 +0200

    TempChanges

--- entity.py
+++ entity.py
@@ -11,6 +11,7 @@ class Entity(object):
         self.directions = {UP: Vector2(0, -1), DOWN: Vector2(0, 1),
                            LEFT: Vector2(-1, 0), RIGHT: Vector2(1, 0), STOP: Vector2()}
         self.direction = STOP
+        self.speed =1
         self.setSpeed(100)
         self.radius = 10
         self.collideRadius = 5
