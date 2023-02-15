22c2b48c9ad5b41860b6fe003b3da8c342b965f2
Avruschenko Dmytro <trippyfren@gmail.com>
2023-02-15 16:54:12 +0200

    Add posibility to kill Ghost

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Some info
+#Pacman Project using Pygame library in Python using java)))))))))))
+It is the project for KNU university with meat
--- run.py
+++ run.py
@@ -57,6 +57,8 @@ class GameController(object):
         if pellet:
             self.pellets.numEaten += 1
             self.pellets.pelletList.remove(pellet)
+            if pellet.name == POWERPELLET:
+                self.ghost.startFreight()
 
 
 if __name__ == "__main__":
