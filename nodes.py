14fe7b43f05608d826892df627934dc8b68edaed
Avruschenko Dmytro <trippyfren@gmail.com>
2023-02-16 12:28:08 +0200

    Adding check of denyed nodes

--- nodes.py
+++ nodes.py
@@ -7,6 +7,10 @@ class Node(object):
     def __init__(self, x, y):
         self.position = Vector2(x, y)
         self.neighbors = {UP:None, DOWN:None, LEFT:None, RIGHT:None, PORTAL:None}
+        self.access = {UP: [PACMAN, BLINKY, PINKY, INKY, CLYDE, FRUIT],
+                       DOWN: [PACMAN, BLINKY, PINKY, INKY, CLYDE, FRUIT],
+                       LEFT: [PACMAN, BLINKY, PINKY, INKY, CLYDE, FRUIT],
+                       RIGHT: [PACMAN, BLINKY, PINKY, INKY, CLYDE, FRUIT]}
 
     def render(self, screen):
         for n in self.neighbors.keys():
@@ -15,6 +19,15 @@ class Node(object):
                 line_end = self.neighbors[n].position.asTuple()
                 pygame.draw.line(screen, WHITE, line_start, line_end, 4)
                 pygame.draw.circle(screen, RED, self.position.asInt(), 12)
+
+    def denyAccess(self, direction, entity):
+        if entity.name in self.access[direction]:
+            self.access[direction].remove(entity.name)
+
+    def allowAccess(self, direction, entity):
+        if entity.name not in self.access[direction]:
+            self.access[direction].append(entity.name)
+
 
 class NodeGroup(object):
     def __init__(self, level):
