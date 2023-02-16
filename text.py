815b8cc9c05fb1a0862d19c0bc4e2d77e486b8fa
Avruschenko Dmytro <trippyfren@gmail.com>
2023-02-16 14:05:58 +0200

    Create text class for showing text in game area

---
+++ text.py
@@ -0,0 +1,41 @@
+import pygame
+from vector import Vector2
+from constants import *
+
+class Text(object):
+    def __init__(self, text, color, x, y, size, time=None, id=None, visible=True):
+        self.id = id
+        self.text = text
+        self.color = color
+        self.size = size
+        self.visible = visible
+        self.position = Vector2(x, y)
+        self.timer = 0
+        self.lifespan = time
+        self.label = None
+        self.destroy = False
+        self.setupFont("PressStart2P-Regular.ttf")
+        self.createLabel()
+
+    def setupFont(self, fontpath):
+        self.font = pygame.font.Font(fontpath, self.size)
+
+    def createLabel(self):
+        self.label = self.font.render(self.text, 1, self.color)
+
+    def setText(self, newtext):
+        self.text = str(newtext)
+        self.createLabel()
+
+    def update(self, dt):
+        if self.lifespan is not None:
+            self.timer += dt
+            if self.timer >= self.lifespan:
+                self.timer = 0
+                self.lifespan = None
+                self.destroy = True
+
+    def render(self, screen):
+        if self.visible:
+            x, y = self.position.asTuple()
+            screen.blit(self.label, (x, y))
